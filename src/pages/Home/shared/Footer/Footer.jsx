b83c04bc1102c531461faf56d50c77b5e16389de
Easy Learn Institute <easylearnwm02@gmail.com>
2026-02-10 15:21:27 +0600

    Added Navbar and footer

--- src/pages/Home/shared/Footer/Footer.jsx
+++ src/pages/Home/shared/Footer/Footer.jsx
@@ -1,60 +1,41 @@
 import React from 'react';
+import logo from '../../../../assets/Logo.PNG'
+import { Link } from 'react-router';
 
 const Footer = () => {
     return (
-        <footer className="footer sm:footer-horizontal bg-base-300 text-base-content p-10">
-            <nav>
-                <h6 className="footer-title">Services</h6>
-                <a className="link link-hover">Branding</a>
-                <a className="link link-hover">Design</a>
-                <a className="link link-hover">Marketing</a>
-                <a className="link link-hover">Advertisement</a>
-            </nav>
-            <nav>
-                <h6 className="footer-title">Company</h6>
-                <a className="link link-hover">About us</a>
-                <a className="link link-hover">Contact</a>
-                <a className="link link-hover">Jobs</a>
-                <a className="link link-hover">Press kit</a>
-            </nav>
-            <nav>
-                <h6 className="footer-title">Social</h6>
-                <div className="grid grid-flow-col gap-4">
-                    <a>
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="24"
-                            height="24"
-                            viewBox="0 0 24 24"
-                            className="fill-current">
-                            <path
-                                d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
-                        </svg>
-                    </a>
-                    <a>
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="24"
-                            height="24"
-                            viewBox="0 0 24 24"
-                            className="fill-current">
-                            <path
-                                d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
-                        </svg>
-                    </a>
-                    <a>
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="24"
-                            height="24"
-                            viewBox="0 0 24 24"
-                            className="fill-current">
-                            <path
-                                d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
-                        </svg>
-                    </a>
+        <footer className="bg-accent mt-20 py-10">
+            <div className='mx-auto max-w-7xl w-11/12'>
+                <div className='grid grid-cols-1 md:grid-cols-3 gap-10 text-center md:text-left'>
+                    <div className='flex flex-col gap-2 justify-center items-center md:items-start'>
+                        <Link to="/" className="text-xl">
+                            <img src={logo} alt="" className='w-14 h-14' />
+                        </Link>
+                        <div className='mt-10'>
+                            <p className='font-bold mb-3'>Join the community</p>
+                            <p className='text-sm'>Learn about new Skills and discounts</p>
+                        </div>
+                    </div>
+                    <div className='flex flex-col gap-2'>
+                        <h6 className="font-bold">Company</h6>
+                        <Link to="/about" className="link link-hover">About</Link>
+                        <Link to="/courses" className="link link-hover">Courses</Link>
+                        <Link to="/admission" className="link link-hover">Admission</Link>
+                        <Link to="/webinar" className="link link-hover">Webinar</Link>
+                        <Link to="/success-story" className="link link-hover">Success Story</Link>
+                    </div>
+                    <div className='flex flex-col gap-2'>
+                        <h6 className="font-bold">Address</h6>
+                        <p className='text-sm'>Email: easylearninstitute64@gmail.com</p>
+                        <p className='text-sm'>Phone: +8809617449909</p>
+                        <p className='text-sm'>Hazibag Dhal, Joydebpur, Gazipur,Dhaka.</p>
+                    </div>
                 </div>
-            </nav>
+                <div className='flex flex-col md:flex-row gap-4 justify-center items-center mt-10 pt-10 border-t border-primary/10'>
+                    <Link to="/" className="font-bold text-xl hover:text-secondary">EasyLearn Institute</Link>
+                    <p className='text-sm'>@ Copyright 2024 | All rights reserved.</p>
+                </div>
+            </div>
         </footer>
     );
 };
--- src/pages/Home/shared/Navbar/Navbar.jsx
+++ src/pages/Home/shared/Navbar/Navbar.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link, NavLink } from 'react-router';
+import logo from '../../../../assets/Logo.PNG'
+import './Navbar.css'
 
 const Navbar = () => {
     const links = <>
@@ -11,27 +13,31 @@ const Navbar = () => {
         <li><NavLink to='/success-stories' className="rounded-xl font-semibold px-4">Success Stories</NavLink></li>
     </>
     return (
-        <div className="navbar bg-base-100 shadow-sm">
-            <div className="navbar-start">
-                <div className="dropdown">
-                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+        <div className="bg-accent text-primary shadow-sm">
+            <div className='navbar max-w-7xl  mx-auto w-11/12 px-0'>
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+                        </div>
+                        <ul
+                            tabIndex="-1"
+                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                            {links}
+                        </ul>
                     </div>
-                    <ul
-                        tabIndex="-1"
-                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                    <Link to="/" className="text-xl">
+                        <img src={logo} alt="" className='w-12 h-12' />
+                    </Link>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">
                         {links}
                     </ul>
                 </div>
-                <Link to="/" className="btn btn-ghost text-xl">Easy Learn Institute</Link>
-            </div>
-            <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1">
-                    {links}
-                </ul>
-            </div>
-            <div className="navbar-end">
-                <Link to="/login" className="btn">Login</Link>
+                <div className="navbar-end">
+                    <Link to="/login" className="btn btn-primary">Login</Link>
+                </div>
             </div>
         </div>
     );
